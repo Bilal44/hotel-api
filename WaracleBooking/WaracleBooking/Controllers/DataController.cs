14bef0c9e44fbced2317067fc81d3a785db31cea
Bilal44 <bilalahmed90@yahoo.com>
2025-08-29 03:50:06 +0100

    Add controllers for hotel and data seeding

---
+++ WaracleBooking/WaracleBooking/Controllers/DataController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using WaracleBooking.Filters;
+using WaracleBooking.Persistence.Repositories.Interfaces;
+
+namespace WaracleBooking.Controllers;
+
+[Produces(MediaTypeNames.Application.Json)]
+[Route("api/data")]
+[ServiceFilter(typeof(ApiExceptionFilter))]
+[ApiController]
+public class DataController (IDataRepository dataRepository) : ControllerBase
+{
+    /// <summary>
+    /// Seeds the database with initial data for hotels, rooms and bookings.
+    /// </summary>
+    /// <returns>A <see cref="CreatedResult"/> indicating the seeding operation was successful.</returns>
+    [HttpPost]
+    public async Task<IActionResult> SeedData()
+    {
+        await dataRepository.SeedData();
+        return Created();
+    }
+    
+    /// <summary>
+    /// Clears all hotel, room and booking data from the database.
+    /// </summary>
+    /// <returns>An <see cref="OkResult"/> indicating the reset operation was successful.</returns>
+    [HttpDelete]
+    public async Task<IActionResult> ResetData()
+    {
+        await dataRepository.ClearData();
+        return Ok();
+    }
+}
---
+++ WaracleBooking/WaracleBooking/Controllers/HotelController.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using WaracleBooking.Filters;
+using WaracleBooking.Mapping;
+using WaracleBooking.Models.Domain;
+using WaracleBooking.Models.Domain.Booking.Validators;
+using WaracleBooking.Services.Interfaces;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+
+namespace WaracleBooking.Controllers;
+
+[Produces(MediaTypeNames.Application.Json)]
+[Route("api/hotels")]
+[ServiceFilter(typeof(ApiExceptionFilter))]
+[ApiController]
+public class HotelController(IBookingService bookingService) : ControllerBase
+{
+    /// <summary>
+    /// Retrieves hotels that fully or partially match the specified name.
+    /// The name matching is case-insensitive.
+    /// </summary>
+    /// <param name="name">Optional: The name or partial name of the hotel.</param>
+    /// <param name="cancellationToken">Token to cancel the operation.</param>
+    /// <returns>A list of matching hotels.</returns>
+    /// <remarks>
+    /// If <paramref name="name"/> is null or whitespace, the method returns all hotels.
+    /// </remarks>
+    [ProducesResponseType(typeof(List<Hotel>), Status200OK)]
+    [ProducesResponseType(Status500InternalServerError)]
+    [HttpGet]
+    public async Task<IActionResult> GetHotelByName(
+        [FromQuery] string? name,
+        CancellationToken cancellationToken) =>
+          Ok(BookingMapper.MapToHotels(await bookingService.GetHotelsByNameAsync(name, cancellationToken)));
+    
+    /// <summary>
+    /// Retrieves available rooms for a hotel within a specified date range and guest count.
+    /// </summary>
+    /// <param name="id">The ID of the hotel.</param>
+    /// <param name="from">Start date of the booking.</param>
+    /// <param name="to">End date of the booking.</param>
+    /// <param name="numberOfGuests">Number of guests to accommodate.</param>
+    /// <param name="cancellationToken">Token to cancel the operation.</param>
+    /// <returns>A list of available rooms.</returns>
+    [ProducesResponseType(typeof(List<Room>), Status200OK)]
+    [ProducesResponseType(Status400BadRequest)]
+    [ProducesResponseType(Status500InternalServerError)]
+    [HttpGet("{id:int}/rooms")]
+    public async Task<IActionResult> GetAvailableRooms(
+        [FromRoute] int id,
+        DateOnly from,
+        DateOnly to,
+        int numberOfGuests,
+        CancellationToken cancellationToken)
+    {
+        var error = DateAndGuestInputValidator.Validate(from, to, numberOfGuests);
+        if (error is not null)
+            return BadRequest(error);
+        
+        return Ok(BookingMapper.MapToRooms(
+            await bookingService.GetAvailableRoomsAsync(id, from, to, numberOfGuests, cancellationToken)));
+    }
+}
